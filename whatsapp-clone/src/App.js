0012d171d663fd36768003c724f8a1f22d57b696
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-13 23:23:46 +0530

    Update App.js

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -2,15 +2,28 @@ import React from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
       <div className="app_body">
-        {/* Sidebar */}
-        <Sidebar />
-        {/* Chat */}
-        <Chat />
+        <Router>
+          <Sidebar />
+          <Switch>
+            <Route path="/rooms/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <Chat />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </div>
   );
