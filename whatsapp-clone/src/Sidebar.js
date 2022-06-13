6c420c5df9bb7678672656f50cf9b9de60f5ecdb
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-13 18:37:49 +0530

    Update Sidebar.js

--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -31,7 +31,7 @@ function Sidebar() {
             </div>
         </div>
         <div className="sidebar_chats">
-            <SidebarChat />
+            <SidebarChat addNewChat/>
             <SidebarChat />
             <SidebarChat />
             <SidebarChat />
