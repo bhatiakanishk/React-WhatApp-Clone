dfc26b68c0f806461d1269bee77947d6ae75172d
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-13 18:37:44 +0530

    Update SidebarChat.js

--- whatsapp-clone/src/SidebarChat.js
+++ whatsapp-clone/src/SidebarChat.js
@@ -2,12 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { Avatar } from "@mui/material";
 import './Sidebar.css';
 
-function SidebarChat() {
+function SidebarChat({addNewChat}) {
     const [seed, setSeed] = useState(''); 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000));
-    }, [])
-  return (
+    }, []);
+
+    const createChat = () => {
+        const roomName = prompt("Please enter name for chat");
+        if (roomName) {
+            // database
+        }
+    };
+
+  return !addNewChat ? (
     <div className="sidebar-chat">
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
         <div className="sidebarChat_info">
@@ -15,7 +23,11 @@ function SidebarChat() {
             <p>Last message...</p>
         </div>
     </div>
-  )
+  ): (
+    <div onClick={createChat} className="sidebarChat">
+        <h2>Add new chat</h2>
+    </div>
+  );
 }
 
 export default SidebarChat
