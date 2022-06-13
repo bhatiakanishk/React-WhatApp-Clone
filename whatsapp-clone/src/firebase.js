f650d520f514d4e23d1f678e736a83d802b44157
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-13 20:10:28 +0530

    Update firebase.js

--- whatsapp-clone/src/firebase.js
+++ whatsapp-clone/src/firebase.js
@@ -1,4 +1,4 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
 const firebaseConfig = {
     apiKey: "AIzaSyDwDmsimK7sgt1mVJlFbRiIPI56Lxg4sPQ",
     authDomain: "whatsapp-clone-45f78.firebaseapp.com",
@@ -8,3 +8,11 @@ const firebaseConfig = {
     appId: "1:193336539138:web:63caca960e47917c8c4f1b",
     measurementId: "G-K9QTH9MCQN"
   };
+
+  const firebaseApp = firebase.initialize(firebaseConfig);
+  const db = firebase.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;
