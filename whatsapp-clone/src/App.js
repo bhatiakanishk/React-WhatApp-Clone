01a7a09c0fd208846a53555d9ecba55d6f4a2522
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-14 17:54:23 +0530

    Update App.js

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -3,15 +3,11 @@ import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 import Login from './Login';
-import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Switch,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
+import { useStateValue } from './StateProvider';
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [{user}, dispatch] = useStateValue();
   return (
     <div className="app">
       {!user ? (
